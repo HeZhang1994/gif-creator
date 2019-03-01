1ceb6dfcdac4d64e1868f7e7d5eebbb3e997487b
He Zhang <zhangheupc@126.com>
2019-03-01 03:42:29 +0000

    Update run_GIFCreator.py

--- run_GIFCreator.py
+++ run_GIFCreator.py
@@ -16,23 +16,25 @@ tag_type = 1  # Set the type of created GIF.
 # tag_type = 1 - Generate GIF image with each frame has same duration.
 # tag_type = 2 - Generate GIF image with each frame has different duration.
 
-
 ## Create GIF image.
 
 if tag_type == 1:
     ## Opt. 1 Generate GIF image with each frame has SAME duration.
+    
     imgGIF = []
     imgNames = sorted((iN for iN in os.listdir(input_path) if iN.endswith(img_extension)))
     # Note: 
-    # The serial number of image name should has same length!!!
-    # Or the sorted() function returns wrong order of images!!!
-    # E.g., _01.png _02.png ... _12.png ...
+    #     The serial number of image name should has same length!!!
+    #     Or the sorted() function returns wrong order of images!!!
+    #     E.g., _01.png _02.png ... _12.png ...
     
     for imgName in imgNames:
         imgGIF.append(imageio.imread(input_path + imgName))
     imageio.mimsave(input_path + 'imgGIF_SAME.gif', imgGIF, duration=duration_time)
+
 elif tag_type == 2:
     ## Opt. 2 Generate GIF image with each frame has DIFF duration.
+    
     input_path = 'Img_Frames/'  # Input image folder.
     img_extension = '.png'  # Format of input image frames.
     duration_time = 0.5  # Duration time of each frame.
